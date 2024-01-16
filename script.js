68ec1cf0d50ae8f4c29f633ff11be8bcd0c692ab
lacertavirdis <zuzanna.przybyl@uczen.zsk.poznan.pl>
2024-01-16 21:24:22 +0100

    full functionality

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let main = document.querySelector("main");
 
 let content = document.querySelector("main>section");
 
+let back = document.querySelectorAll("div > img");
+
 //event listeners
 am.addEventListener("click", function(){
     am.style = "width:85%; height:100px; justify-content:center; flex-direction:column; margin-top:0;";
@@ -22,21 +24,62 @@ am.addEventListener("click", function(){
 });
 
 ed.addEventListener("click", function(){
+    ed.style = "width:85%; height:100px; justify-content:center; flex-direction:column; margin-top:0;";
+    content.style.display = "flex";
+    am.style.display = "none", sk.style.display = "none", ms.style.display="none", fl.style.display="none", it.style.display = "none";
     
+    content.innerHTML = "I am a student at Zespół Szkół Komunikacji in Poznań,<br>"
+    +"where I study computer science by gaining theoretical as well as practical knowledge.<br>"
+    +""
+    +"I am hopeful to be an accomplished programmer someday.";
 });
 
 sk.addEventListener("click", function(){
+    sk.style = "width:85%; height:100px; justify-content:center; flex-direction:column; margin-top:0;";
+    content.style.display = "flex";
+    am.style.display = "none", ed.style.display = "none", ms.style.display="none", fl.style.display="none", it.style.display = "none";
     
+    content.innerHTML = "I am a student at Zespół Szkół Komunikacji in Poznań,<br>"
+    +"where I study computer science by gaining theoretical as well as practical knowledge.<br>"
+    +""
+    +"I am hopeful to be an accomplished programmer someday.";
 });
 
 ms.addEventListener("click", function(){
+    ms.style = "width:85%; height:100px; justify-content:center; flex-direction:column; margin-top:0;";
+    content.style.display = "flex";
+    ed.style.display = "none", sk.style.display = "none", am.style.display="none", fl.style.display="none", it.style.display = "none";
     
+    content.innerHTML = "I am a student at Zespół Szkół Komunikacji in Poznań,<br>"
+    +"where I study computer science by gaining theoretical as well as practical knowledge.<br>"
+    +""
+    +"I am hopeful to be an accomplished programmer someday.";
 });
 
 fl.addEventListener("click", function(){
+    fl.style = "width:85%; height:100px; justify-content:center; flex-direction:column; margin-top:0;";
+    content.style.display = "flex";
+    ed.style.display = "none", sk.style.display = "none", ms.style.display="none", am.style.display="none", it.style.display = "none";
     
+    content.innerHTML = "I am a student at Zespół Szkół Komunikacji in Poznań,<br>"
+    +"where I study computer science by gaining theoretical as well as practical knowledge.<br>"
+    +""
+    +"I am hopeful to be an accomplished programmer someday.";
 });
 
 it.addEventListener("click", function(){
+    it.style = "width:85%; height:100px; justify-content:center; flex-direction:column; margin-top:0;";
+    content.style.display = "flex";
+    ed.style.display = "none", sk.style.display = "none", ms.style.display="none", fl.style.display="none", am.style.display = "none";
     
+    content.innerHTML = "I am a student at Zespół Szkół Komunikacji in Poznań,<br>"
+    +"where I study computer science by gaining theoretical as well as practical knowledge.<br>"
+    +""
+    +"I am hopeful to be an accomplished programmer someday.";
 });
+
+for(var i = 0; i < back.length; i++) {
+    back[i].addEventListener("click", function(){
+        window.location.href = "index.html";
+    });
+}
