60cbfb9dae2cc125053aae43590fddccff8b5004
lacertavirdis <zuzanna.przybyl@uczen.zsk.poznan.pl>
2024-01-22 12:02:30 +0100

    g

--- script.js
+++ script.js
@@ -36,8 +36,7 @@ ed.addEventListener("click", function(){
     content.style.fontSize = "175%";
     am.style.display = "none", sk.style.display = "none", ms.style.display="none", fl.style.display="none", it.style.display = "none";
     
-    content.innerHTML = "Zespół Szkół Komunikacji im. Hipolita Cegielskiego w Poznaniu | 2021 - now<br><br>"
-    +"Szkoła Podstawowa nr 1 w Mosinie | 2013 - 2021";
+    content.innerHTML = "Zespół Szkół Komunikacji im. Hipolita Cegielskiego w Poznaniu | 2021 - now";
 });
 
 sk.addEventListener("click", function(){
@@ -70,12 +69,14 @@ ms.addEventListener("click", function(){
     content.style.fontSize = "145%";
     ed.style.display = "none", sk.style.display = "none", am.style.display="none", fl.style.display="none", it.style.display = "none";
     
-    content.innerHTML = "Thanks to the exchanges I had been a part of, I work great in a team. "
-    +"More often than not, I tend to take on a role of the teamleader and delegate the work among the team members. "
-    +"People say that I am an open-minded person and kind to everyone in my community, I believe that's true. "
-    +"I make connections very easily and everywhere I go, I always meet someone new and I get to know them very well in a little time. "
-    +"Nowadays, It is not common to know someone who is well behaved and polite, but I am that person. "
-    +"For last, I always strive for improvement, whether it's programming or my hobbies, I like to try and be the best version of myself that I can be. "
+    content.innerHTML = "<ul>"
+    +"<li>Skillful team work</li>"
+    +"<li>Being a teamleader</li>"
+    +"<li>Open-mindedness</li>"
+    +"<li>Making connections very easily</li>"
+    +"<li>Being well behaved and polite</li>"
+    +"<li>Always striving for improvement</li>"
+    +"</ul>"
     ;
 });
 
@@ -87,7 +88,7 @@ fl.addEventListener("click", function(){
     content.style.fontSize = "250%";
     ed.style.display = "none", sk.style.display = "none", ms.style.display="none", am.style.display="none", it.style.display = "none";
     
-    content.innerHTML = "Polish - native<br><br>English - C1<br><br>German - A2+/B1";
+    content.innerHTML = "Polish - native<br><br>English - C1<br><br>German - B1";
 });
 
 it.addEventListener("click", function(){
@@ -97,12 +98,14 @@ it.addEventListener("click", function(){
     content.style.display = "flex";
     ed.style.display = "none", sk.style.display = "none", ms.style.display="none", fl.style.display="none", am.style.display = "none";
     
-    content.innerHTML = "Since I can remember I have always been interested in music. I've played the piano for several years, "
-    +"I listen to music every single second I can and two years ago i started playing the guitar which is going really well, "
-    +"considering the fact that I'm a selt-taught. In my free time I also like to watch TV series, my favorite is Friends, "
-    +"The Big Bang Theory, How I Met Your Mother and The Office. A year ago I discovered my passion for crocheting. "
-    +"When I need a break from reality I like to cuddle up with a book and sink into the story, my favorite books include "
-    +"The Island of Missing Trees, And Then There Were None, The Bell Jar, The Handmaid's Tale and The Little Prince."
+    content.innerHTML = "<ul>"
+    +"<li>Music</li>"
+    +"<li>Watching TV series (my favorites: Friends, TBBT, HIMYM, The Office)</li>"
+    +"<li>Crocheting</li>"
+    +"<li>Reading books (my favorites: And Then There Were None, The Bell Jar, The Handmaid's Tale)</li>"
+    +"<li>Spending time with my friends and family</li>"
+    +"<li>Learning languages</li>"
+    +"</ul>"
     ;
 });
 
